bef12ec6ef3cd29b55d85778415905a07857a5de
Quentin Devillers <devi.quentin@gmail.com>
2022-12-19 13:00:44 +0100

    dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import Card from "./components/card/card"
 import CardType from "./components/card/cardType"
+import Competences from "./components/competences/Competences"
 import "./styles/App.sass"
 
 function App() {
@@ -9,31 +10,47 @@ function App() {
       <h1>Quentin Devillers</h1>
 
       {/* MES PROJETS */}
-      <h2>Mes projets</h2>
-      <Card
-        data={{
-          title: "Nom du projet",
-          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...",
-          image: "https://loremflickr.com/640/360",
-          tags: ["Javascript", "React", "SASS"],
-          href: "#",
-          activated: true,
-        }}
-      />
+      <section>
+        <h2>Mes projets</h2>
+        <Card
+          data={{
+            title: "Overlore",
+            description: "Site regroupant héros, histoires et skins du jeu vidéo Overwatch. API php. 2ème développeur Astoryon",
+            image: "./images/projets/overlore.jpg",
+            tags: ["Typescript", "React", "Api PHP"],
+            href: "https://overlore.netlify.app",
+            activated: true,
+          }}
+        />
 
-      <Card
-        data={{
-          title: "Nom du projet",
-          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...",
-          image: "https://loremflickr.com/640/360",
-          tags: ["Javascript", "React", "SASS"],
-          href: "#",
-          activated: true,
-        }}
-      />
+        <Card
+          data={{
+            title: "Nom du projet",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...",
+            image: "https://loremflickr.com/640/360",
+            tags: ["Javascript", "React", "SASS"],
+            href: "#",
+            activated: true,
+          }}
+        />
+
+        <Card
+          data={{
+            title: "Nom du projet",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...",
+            image: "https://loremflickr.com/640/360",
+            tags: ["Javascript", "React", "SASS"],
+            href: "#",
+            activated: true,
+          }}
+        />
+      </section>
 
       {/* MES COMPETENCES */}
-      <h2>MES COMPETENCES</h2>
+      <section>
+        <h2>Mes compétences</h2>
+        <Competences />
+      </section>
 
       {/* FOOTER */}
       <footer className="App__footer"></footer>
---
+++ src/components/competences/Competences.tsx
@@ -0,0 +1,13 @@
+const Competences = () => {
+  return (
+    <article className="competences">
+      <figure className="competences__logo">a</figure>
+      <figure className="competences__logo">b</figure>
+      <figure className="competences__logo"></figure>
+      <figure className="competences__logo"></figure>
+      <figure className="competences__logo"></figure>
+    </article>
+  )
+}
+
+export default Competences
